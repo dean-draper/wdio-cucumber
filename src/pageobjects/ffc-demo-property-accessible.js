6b387195e23c2078ced2600be39a9976b405ecb3
unifono <dean.draper@me.com>
2019-12-12 15:03:18 +0000

    page object files

--- src/pageobjects/ffc-demo-property-accessible.js
+++ src/pageobjects/ffc-demo-property-accessible.js
@@ -1,28 +1,26 @@
-import Page from './page';
-import utl   from '../../utilities/common-utilities';
+import Page from './page'
+import utl from '../../utilities/common-utilities'
 
 class propertyAccessible extends Page {
+  /**
+  * define elements
+  */
 
-    /**
-    * define elements
-    */
+  get serviceName () { return $('//.govuk-header__link--service-name') }
+  get headingQuestion () { return $('//.govuk-fieldset__heading') }
+  get questionHint () { return $('//#accessible-hint') }
+  get yesRadioButton () { return $('//*[(@id = "accessible")]') }
+  get noRadioButton () { return $('//*[(@id = "accessible-2")]') }
+  get saveAndContinueButton () { return $('.govuk-button') }
 
-
-    get serviceName()   { return $('//.govuk-header__link--service-name'); }
-    get headingQuestion()   { return $('//.govuk-fieldset__heading'); }
-    get questionHint()     { return $('//#accessible-hint'); }
-    get yesRadioButton() { return $('//*[(@id = "accessible")]');}  
-    get noRadioButton() { return $('//*[(@id = "accessible-2")]');}  
-    get saveAndContinueButton()    { return $('.govuk-button');}
-
-    /**
+  /**
      * define or overwrite page methods
      */
-    open () {
-        super.open('')       
-        browser.pause(3000);
-    }
-    /**
+  open () {
+    super.open('')
+    browser.pause(3000)
+  }
+  /**
      * your page specific methods
      */
 }
--- src/steps/given.js
+++ src/steps/given.js
@@ -1,127 +1,127 @@
-import { Given } from 'cucumber';
+import { Given } from 'cucumber'
 
-import checkContainsAnyText from '../support/check/checkContainsAnyText';
-import checkIsEmpty from '../support/check/checkIsEmpty';
-import checkContainsText from '../support/check/checkContainsText';
-import checkCookieContent from '../support/check/checkCookieContent';
-import checkCookieExists from '../support/check/checkCookieExists';
-import checkDimension from '../support/check/checkDimension';
-import checkElementExists from '../support/check/checkElementExists';
-import checkEqualsText from '../support/check/checkEqualsText';
-import checkModal from '../support/check/checkModal';
-import checkOffset from '../support/check/checkOffset';
-import checkProperty from '../support/check/checkProperty';
-import checkSelected from '../support/check/checkSelected';
-import checkTitle from '../support/check/checkTitle';
-import checkUrl from '../support/check/checkURL';
-import closeAllButFirstTab from '../support/action/closeAllButFirstTab';
-import compareText from '../support/check/compareText';
-import isEnabled from '../support/check/isEnabled';
-import isDisplayed from '../support/check/isDisplayed';
-import openWebsite from '../support/action/openWebsite';
-import setWindowSize from '../support/action/setWindowSize';
-
-Given(
-    /^I open the (url|site) "([^"]*)?"$/,
-    openWebsite
-);
+import checkContainsAnyText from '../support/check/checkContainsAnyText'
+import checkIsEmpty from '../support/check/checkIsEmpty'
+import checkContainsText from '../support/check/checkContainsText'
+import checkCookieContent from '../support/check/checkCookieContent'
+import checkCookieExists from '../support/check/checkCookieExists'
+import checkDimension from '../support/check/checkDimension'
+import checkElementExists from '../support/check/checkElementExists'
+import checkEqualsText from '../support/check/checkEqualsText'
+import checkModal from '../support/check/checkModal'
+import checkOffset from '../support/check/checkOffset'
+import checkProperty from '../support/check/checkProperty'
+import checkSelected from '../support/check/checkSelected'
+import checkTitle from '../support/check/checkTitle'
+import checkUrl from '../support/check/checkURL'
+import closeAllButFirstTab from '../support/action/closeAllButFirstTab'
+import compareText from '../support/check/compareText'
+import isEnabled from '../support/check/isEnabled'
+import isDisplayed from '../support/check/isDisplayed'
+import openWebsite from '../support/action/openWebsite'
+import setWindowSize from '../support/action/setWindowSize'
 
 Given(
-    /^the element "([^"]*)?" is( not)* displayed$/,
-    isDisplayed
-);
+  /^I open the (url|site) "([^"]*)?"$/,
+  openWebsite
+)
 
 Given(
-    /^the element "([^"]*)?" is( not)* enabled$/,
-    isEnabled
-);
+  /^the element "([^"]*)?" is( not)* displayed$/,
+  isDisplayed
+)
 
 Given(
-    /^the element "([^"]*)?" is( not)* selected$/,
-    checkSelected
-);
+  /^the element "([^"]*)?" is( not)* enabled$/,
+  isEnabled
+)
 
 Given(
-    /^the checkbox "([^"]*)?" is( not)* checked$/,
-    checkSelected
-);
+  /^the element "([^"]*)?" is( not)* selected$/,
+  checkSelected
+)
 
 Given(
-    /^there is (an|no) element "([^"]*)?" on the page$/,
-    checkElementExists
-);
+  /^the checkbox "([^"]*)?" is( not)* checked$/,
+  checkSelected
+)
 
 Given(
-    /^the title is( not)* "([^"]*)?"$/,
-    checkTitle
-);
+  /^there is (an|no) element "([^"]*)?" on the page$/,
+  checkElementExists
+)
 
 Given(
-    /^the element "([^"]*)?" contains( not)* the same text as element "([^"]*)?"$/,
-    compareText
-);
+  /^the title is( not)* "([^"]*)?"$/,
+  checkTitle
+)
 
 Given(
-    /^the (button|element) "([^"]*)?"( not)* matches the text "([^"]*)?"$/,
-    checkEqualsText
-);
+  /^the element "([^"]*)?" contains( not)* the same text as element "([^"]*)?"$/,
+  compareText
+)
 
 Given(
-    /^the (button|element|container) "([^"]*)?"( not)* contains the text "([^"]*)?"$/,
-    checkContainsText
-);
+  /^the (button|element) "([^"]*)?"( not)* matches the text "([^"]*)?"$/,
+  checkEqualsText
+)
 
 Given(
-    /^the (button|element) "([^"]*)?"( not)* contains any text$/,
-    checkContainsAnyText
-);
+  /^the (button|element|container) "([^"]*)?"( not)* contains the text "([^"]*)?"$/,
+  checkContainsText
+)
 
 Given(
-    /^the (button|element) "([^"]*)?" is( not)* empty$/,
-    checkIsEmpty
-);
+  /^the (button|element) "([^"]*)?"( not)* contains any text$/,
+  checkContainsAnyText
+)
 
 Given(
-    /^the page url is( not)* "([^"]*)?"$/,
-    checkUrl
-);
+  /^the (button|element) "([^"]*)?" is( not)* empty$/,
+  checkIsEmpty
+)
 
 Given(
-    /^the( css)* attribute "([^"]*)?" from element "([^"]*)?" is( not)* "([^"]*)?"$/,
-    checkProperty
-);
+  /^the page url is( not)* "([^"]*)?"$/,
+  checkUrl
+)
 
 Given(
-    /^the cookie "([^"]*)?" contains( not)* the value "([^"]*)?"$/,
-    checkCookieContent
-);
+  /^the( css)* attribute "([^"]*)?" from element "([^"]*)?" is( not)* "([^"]*)?"$/,
+  checkProperty
+)
 
 Given(
-    /^the cookie "([^"]*)?" does( not)* exist$/,
-    checkCookieExists
-);
+  /^the cookie "([^"]*)?" contains( not)* the value "([^"]*)?"$/,
+  checkCookieContent
+)
 
 Given(
-    /^the element "([^"]*)?" is( not)* ([\d]+)px (broad|tall)$/,
-    checkDimension
-);
+  /^the cookie "([^"]*)?" does( not)* exist$/,
+  checkCookieExists
+)
 
 Given(
-    /^the element "([^"]*)?" is( not)* positioned at ([\d]+)px on the (x|y) axis$/,
-    checkOffset
-);
+  /^the element "([^"]*)?" is( not)* ([\d]+)px (broad|tall)$/,
+  checkDimension
+)
 
 Given(
-    /^I have a screen that is ([\d]+) by ([\d]+) pixels$/,
-    setWindowSize
-);
+  /^the element "([^"]*)?" is( not)* positioned at ([\d]+)px on the (x|y) axis$/,
+  checkOffset
+)
 
 Given(
-    /^I have closed all but the first (window|tab)$/,
-    closeAllButFirstTab
-);
+  /^I have a screen that is ([\d]+) by ([\d]+) pixels$/,
+  setWindowSize
+)
 
 Given(
-    /^a (alertbox|confirmbox|prompt) is( not)* opened$/,
-    checkModal
-);
+  /^I have closed all but the first (window|tab)$/,
+  closeAllButFirstTab
+)
+
+Given(
+  /^a (alertbox|confirmbox|prompt) is( not)* opened$/,
+  checkModal
+)
